b4568ec2a8122c99cc1b0caad843fd48bc0f0bad
Parker Hudson <gphudson97@hotmail.com>
2021-12-02 13:54:02 -0500

    Formatting

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -17,24 +17,25 @@ namespace SportsStore
 {
     public class Startup
     {
-        IConfigurationRoot Configuration;
+        private IConfigurationRoot Configuration;
+
         public Startup(IWebHostEnvironment env)
         {
             Configuration = new ConfigurationBuilder()
-            .SetBasePath(env.ContentRootPath)
-            .AddJsonFile("appsettings.json").Build();
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json").Build();
         }
 
         // This method gets called by the runtime. Use this method to add services to the 
-       // container.
+        // container.
         // For more information on how to configure your application, visit 
-       // https://go.microsoft.com/fwlink/?LinkID=398940 
+        // https://go.microsoft.com/fwlink/?LinkID=398940 
         public void ConfigureServices(IServiceCollection services)
         {
             //services.AddTransient<IProductRepository, FakeProductRepository>(); 
 
             services.AddDbContext<ApplicationDbContext>(options =>
-options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
@@ -47,7 +48,6 @@ options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
                 .AddNewtonsoftJson();
             services.AddMemoryCache();
             services.AddSession();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request 
@@ -59,8 +59,8 @@ options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             app.UseStaticFiles();
             app.UseAuthentication();
             app.UseSession();
-            app.UseMvc(routes => {
-
+            app.UseMvc(routes =>
+            {
                 // routes.MapRoute(
                 //     name: null,
                 // template: "{category}/Page{page:int}",
@@ -83,15 +83,14 @@ options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
                 // routes.MapRoute(name: null, template: "{controller}/{action}/{id?}");
 
                 routes.MapRoute(
-                    name: "pagination",
-                    template: "Products/List/Page{page}",
-                    defaults: new { Controller = "Product", action = "List" });
+                    "pagination",
+                    "Products/List/Page{page}",
+                    new {Controller = "Product", action = "List"});
 
                 routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Product}/{action=List}/{id?}");
+                    "default",
+                    "{controller=Product}/{action=List}/{id?}");
             });
-
         }
     }
 }
