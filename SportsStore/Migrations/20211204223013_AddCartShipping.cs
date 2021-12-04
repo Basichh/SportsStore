2ec085ad443724cbf5641a4bce800ba8a479abf1
Nick <n00961014@unf.edu>
2021-12-04 17:50:10 -0500

    2

---
+++ SportsStore/Migrations/20211204223013_AddCartShipping.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SportsStore.Migrations
+{
+    public partial class AddCartShipping : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Shipping",
+                table: "CartLine",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "ShownTax",
+                table: "CartLine",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Tax",
+                table: "CartLine",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Shipping",
+                table: "CartLine");
+
+            migrationBuilder.DropColumn(
+                name: "ShownTax",
+                table: "CartLine");
+
+            migrationBuilder.DropColumn(
+                name: "Tax",
+                table: "CartLine");
+        }
+    }
+}
